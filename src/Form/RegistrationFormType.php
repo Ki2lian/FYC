a3f902916426dbb48abccaef110b26581c5c4c9b
Killian <kignon12@gmail.com>
2022-01-10 16:47:51 +0100

    First commit

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'error_bubbling' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un pseudo',
+                    ])
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'error_bubbling' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un e-mail',
+                    ])
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'error_bubbling' => true,
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Regex([
+                        "pattern" => '/^(?=\P{Ll}*\p{Ll})(?=\P{Lu}*\p{Lu})(?=\P{N}*\p{N})(?=[\p{L}\p{N}]*[^\p{L}\p{N}])[\s\S]{8,4096}$/',
+                        "match" => true,
+                        "message" => "Le mot de passe doit contenir :
+                        au moins 1 lettre minuscule,
+                        au moins 1 lettre majuscule,
+                        au moins 1 caractère spécial et votre mot de passe doit comporter au moins 8 caractères."
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
